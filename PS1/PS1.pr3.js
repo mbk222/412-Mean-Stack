3112fd9488cb7a287ecad02d09fb1852e70757c9
mbk222 <50961152+mbk222@users.noreply.github.com>
2020-06-06 23:38:02 -0400

    Update PS1.pr3.js

--- PS1/PS1.pr3.js
+++ PS1/PS1.pr3.js
@@ -3,3 +3,7 @@ const pr3 = (str, func) => func(str);
 const e1 = str => str.split(/(?=c)/g);
 
 const e2 = str =>[str, str.replace(/a/g,"A"), str.replace(/a/g,"A").match(/A/g).length.toString(),str.replace(/a/g,"A").length.toString()];
+
+console.log(pr3("supercalifragilisticexpialidocious", e1));
+console.log(pr3("superCalifragilisticexpialidoCious", e1));
+console.log(pr3("supercalifragilisticexpialidocious", e2));
