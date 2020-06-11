a688181995e6a096037638aadd0af81de46c664e
mbk222 <50961152+mbk222@users.noreply.github.com>
2020-06-10 23:35:09 -0400

    Delete PS1.pr1.js

--- PS1/PS1.pr1.js
+++
@@ -1,5 +0,0 @@
-const pr1 = str => str.replace(/[.,\/#@!$%\^&\*;:{}=\-_`~()]/g,"").split('').sort().join('');
-
-console.log(pr1('supercalifragilisticexpialidocious'));
-console.log(pr1('!    supercalifragilist$@icexpialidociou$s'));
-console.log(pr1("!c@bAa."))
