f6e3316d4b4c1c277ecc38a541f62ba38ba606b2
mbk222 <50961152+mbk222@users.noreply.github.com>
2020-07-01 17:05:08 -0400

    Update weather.js

--- routes/weather.js
+++ routes/weather.js
@@ -1,72 +1,46 @@
-const router = require('express').Router();
-const fetch = require('node-fetch');        // to define fetch()
-require('dotenv').config();                 // to read .env 
-
+const router = require("express").Router();
+const fetch = require("node-fetch"); // to define fetch()
+require("dotenv").config(); // to read .env
 
 // REDIS
-const redis = require('redis');
+const redis = require("redis");
 const client = redis.createClient(6739);
 
-router.get('/', (req, res) => {
-    res.render("index", {
-        city: null,
-        des: null,
-        icon: null,
-        temp: null
+router.get("/", async (req, res) => {
+  const city = req.body.city;
+  const url_api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`; // key stored in .env file
+
+  try {
+    // Check the redis store for the data first
+    client.get(city, async (err, data) => {
+      if (data) {
+        return res.status(200).send({
+          error: false,
+          message: `Response from cache ${data}`,
+          result: JSON.parse(data)
+        });
+      } else {
+        // When the data is not found in the cache then we can make request to the server
+
+        const response = await fetch(url_api);
+
+        const data = await response.json();
+
+        // save the record in the cache for subsequent request
+        client.setex(city, 30, JSON.stringify(data));
+
+        // return the result to the client
+        return res.status(200).send({
+          error: false,
+          message: `Response from cache ${data}`,
+          result: data
+        });
+      }
     });
-})
-
-router.post('/', async (req,res) => {
-    const city = req.body.city;
-    const url_api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`; // key stored in .env file
-
-    try {
-        await fetch(url_api) 
-                .then(res => res.json())
-                .then(data => {
-                    if (data.message === "city not found") {
-                        res.render('index', {
-                            city: data.message,
-                            des: null,
-                            icon: null,
-                            temp: null
-                        }) 
-                    } else { // City found
-                        const info = {
-                            "city" : data.name,
-                            "des"  : data.weather[0].description,
-                            "icon" : data.weather[0].icon,
-                            "temp" : data.main.temp
-                        }
-                        const info_JSON = JSON.stringify(info); // creates JSON 
-                        client.setex(city, 30, info_JSON);  // caches JSON to redis only if key does not already exist, expires in 30 seconds
-                        
-                        const city = data.name;
-                        const des = data.weather[0].description;
-                        const icon = data.weather[0].icon;
-                        const temp = kelvinF(data.main.temp).toFixed(1); // API returns temp in K, it is converted to F here.
-
-                        res.render('index', {
-                            //city, des, icon, temp
-                            info_JSON // rendering from JSON
-                        })
-                    }
-                });
-    } catch(err) {
-        res.render('index', {
-            city: 'something went wrong',
-            des: null,
-            icon: null,
-            temp: null
-        })
-    }
-
-    
-})
-
-
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = router;
-
-const kelvinF = (k) => {  return 9/5 * (k - 273.15) + 32 } // K to F conversion
 
