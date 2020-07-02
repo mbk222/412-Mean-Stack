fba743612906927aeb76ef5e6b419e787a5f3244
mbk222 <50961152+mbk222@users.noreply.github.com>
2020-07-01 23:10:51 -0400

    Update app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,28 +9,4 @@ import { Component, SystemJsNgModuleLoader } from '@angular/core';
 export class AppComponent {
   title = 'weather-app-angular';
 
-  data;
-  mockData = [
-    {
-      feels_like: 293.64,
-      humidity: 93,
-      pressure: 1011,
-      temp: 292.56,
-      temp_max: 293.71,
-      temp_min: 291.48,
-    },
-    {
-      feels_like: 293.64,
-      humidity: 93,
-      pressure: 1011,
-      temp: 292.56,
-      temp_max: 293.71,
-      temp_min: 291.48,
-    },
-  ]
-
-  getMock() {
-    this.data = this.mockData
-  }
-
 }
